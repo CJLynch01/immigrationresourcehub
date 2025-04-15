68f7c470657ae0e45a8889cc0906a2153f19307d
CJLynch01 <cjlynch@byui.edu>
2025-04-14 19:21:30 -0600

    Save Server 3000

--- server/index.js
+++ server/index.js
@@ -1,2 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Immigration Resource Hub API is running");
+});
+
+app.listen(PORT, () => {
+    console.log (`Server is running on port ${PORT}`);
+})
