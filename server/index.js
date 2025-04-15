903d78b21e170c7a49a2ba20499a6ce092886eb7
CJLynch01 <cjlynch@byui.edu>
2025-04-15 09:37:24 -0600

    Save. Add auth to index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Immigration Resource Hub API is running");
