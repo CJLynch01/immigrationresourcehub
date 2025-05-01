1b84155a489efcf9a25c8d347e56ef7ac824d0c1
CJLynch01 <cjlynch@byui.edu>
2025-05-01 16:33:03 -0600

    Save

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,20 @@
 const jwt = require("jsonwebtoken");
 
+const jwt = require("jsonwebtoken");
+
 function verifyToken(req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ msg: "Access denied. No token provided." });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.user = { id: decoded._id, role: decoded.role };
     next();
   } catch (err) {
     return res.status(401).json({ msg: "Invalid or expired token" });
   }
 }
+
 
 function isAdmin(req, res, next) {
   if (req.user?.role === "admin") {
