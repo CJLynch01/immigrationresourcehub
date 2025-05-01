8aa8e027acc635dab9fea3444fc0c466d8310491
CJLynch01 <cjlynch@byui.edu>
2025-05-01 17:02:29 -0600

    Save

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-const jwt = require("jsonwebtoken");
-
 function verifyToken(req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ msg: "Access denied. No token provided." });
